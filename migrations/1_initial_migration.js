aef09de638624b5907f06595a6f29c6999aede89
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2024-09-15 17:16:53 +0530

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = async function (_deployer, network, accounts) {
-  // _deployer.deploy(Migrations, { from: accounts[0] });
+  // console.log(require);
 };
