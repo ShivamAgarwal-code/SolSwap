9859211a10740f31bcd8aa620b9df634262c4415
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2024-09-15 17:18:03 +0530

    Update GammaswapLibrary.sol

--- src/contracts/libraries/GammaswapLibrary.sol
+++ src/contracts/libraries/GammaswapLibrary.sol
@@ -78,7 +78,6 @@ library GammaswapLibrary {
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
-    // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
     function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
         require(amountA > 0, 'GammaswapLibrary: INSUFFICIENT_AMOUNT');
         require(reserveA > 0 && reserveB > 0, 'GammaswapV1Library: INSUFFICIENT_LIQUIDITY');
